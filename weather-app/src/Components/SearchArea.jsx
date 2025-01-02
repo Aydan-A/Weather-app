d7a8e36703aaab5503c5e2e13e8fed88799f195c
Aydan Abbaslı <aydan26.abbasova@gmail.com>
2025-01-01 23:43:52 -0500

    Integrate One Call API for hourly forecast data

--- weather-app/src/Components/SearchArea.jsx
+++ weather-app/src/Components/SearchArea.jsx
@@ -18,7 +18,7 @@ export default function SearchArea() {
   };
 
   const fetchWeatherByLocation = async (lat, lon) => {
-    const API_Key = "4767c92c7510a82fd1314991672cdbba";
+    const API_Key = "1669695fdb817cab90ecf96bcc7cf662";
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}&units=metric`;
 
     try {
--- weather-app/src/Components/WeatherContext.js
+++ weather-app/src/Components/WeatherContext.js
@@ -8,9 +8,9 @@ export const WeatherProvider = ({ children }) => {
   const [hourlyData, setHourlyData] = useState(null);
 
   const fetchWeather = async (city) => {
-    const API_Key = "4767c92c7510a82fd1314991672cdbba";
+    const API_Key = "1669695fdb817cab90ecf96bcc7cf662";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`;
-    setError(null);
+
     try {
       const response = await fetch(url);
       if (!response.ok) {
@@ -18,7 +18,14 @@ export const WeatherProvider = ({ children }) => {
       }
       const data = await response.json();
       setWeather(data);
-      setHourlyData(data.hourly)
+      const { lat, lon } = data.coord;
+      const geoUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}&units=metric`;
+      const geoResponse = await fetch(geoUrl);
+      if (!geoResponse.ok) {
+        throw new Error("Failed to fetch weather data");
+      }
+      const geoData = await geoResponse.json();
+      setHourlyData(geoData.hourly);
     } catch (error) {
       setError(error.message);
       console.log("Error fetching weather data:", error);
@@ -27,7 +34,16 @@ export const WeatherProvider = ({ children }) => {
 
   return (
     <WeatherContext.Provider
-      value={{ weather, error, setWeather, setError, fetchWeather, hourlyData,setHourlyData }}>
+      value={{
+        weather,
+        error,
+        setWeather,
+        setError,
+        fetchWeather,
+        hourlyData,
+        setHourlyData,
+      }}
+    >
       {children}
     </WeatherContext.Provider>
   );
